cc6870cc8d9140d16e76dd50fc84bc577319dac9
vmvega <126878140+vmvega@users.noreply.github.com>
2026-05-13 13:06:15 -0500

    config de escucha del servidor mas paquetes para validacion de modelos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(port);
+  console.log(`servidor corriendo http://localhost:${port}`);
 }
-bootstrap();
+void bootstrap();

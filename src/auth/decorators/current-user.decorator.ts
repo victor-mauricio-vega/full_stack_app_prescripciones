c28a5156e84499719c98bd44b7977093a5317e7f
Victor Mauricio Vega <v.mauriciovega.m@gmail.com>
2026-05-15 00:57:39 -0500

    feat: reestructurar el proyecto en backend y frontend

--- src/auth/decorators/current-user.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '@prisma/client';
-
-export const CurrentUser = createParamDecorator(
-  (_: unknown, ctx: ExecutionContext): User => {
-    return ctx.switchToHttp().getRequest().user;
-  },
-);
--- src/auth/decorators/roles.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Role } from '@prisma/client';
-
-export const ROLES = 'roles';
-
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES, roles);
-
-export const IS_PUBLIC = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC, true);
